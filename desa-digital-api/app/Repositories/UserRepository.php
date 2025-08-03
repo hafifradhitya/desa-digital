09b5933e1eabdc107fccb7841e321a7424e15942
hafifradhitya <bangclasher81@gmail.com>
2025-08-03 12:44:37 +0700

    Create Data

--- desa-digital-api/app/Repositories/UserRepository.php
+++ desa-digital-api/app/Repositories/UserRepository.php
@@ -4,6 +4,8 @@ namespace App\Repositories;
 
 use App\Interfaces\UserRepositoryInterface;
 use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\DB;
 
 class UserRepository implements UserRepositoryInterface
 {
@@ -44,4 +46,27 @@ class UserRepository implements UserRepositoryInterface
 
         return $query->paginate($rowPerPage);
     }
+
+    public function create(
+        array $data
+    ){
+        DB::beginTransaction();
+
+        try{
+            $user = new User;
+            $user->name = $data['name'];
+            $user->email = $data['email'];
+            $user->password = bcrypt($data['password']);
+            $user->save();
+
+            DB::commit();
+
+            return $user;
+        } catch(\Exception $e) {
+            DB::rollBack();
+
+            throw new Exception($e->getMessage());
+        }
+    }
+
 }
