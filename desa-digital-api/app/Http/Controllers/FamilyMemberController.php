6f19968a750e3d79faeca5d40eb954fd7715b741
hafifradhitya <bangclasher81@gmail.com>
2025-08-05 16:35:33 +0700

    API Family Member Update Data

--- desa-digital-api/app/Http/Controllers/FamilyMemberController.php
+++ desa-digital-api/app/Http/Controllers/FamilyMemberController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Helpers\ResponseHelper;
 use App\Http\Requests\FamilyMemberStoreRequest;
+use App\Http\Requests\FamilyMemberUpdateRequest;
 use App\Http\Resources\FamilyMemberResource;
 use App\Http\Resources\PaginateResource;
 use App\Interfaces\FamilyMemberRepositoryInterface;
@@ -95,9 +96,28 @@ class FamilyMemberController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(FamilyMemberUpdateRequest $request, string $id)
     {
-        //
+        $request = $request->validated();
+
+        try {
+            $familyMember = $this->familyMemberRepository->getById(
+                $id
+            );
+
+            if(!$familyMember){
+                return ResponseHelper::jsonResponse(false, 'Data Anggota Keluarga Tidak Ditemukan', null, 404);
+            }
+
+            $familyMember = $this->familyMemberRepository->update(
+                $id,
+                $request
+            );
+
+            return ResponseHelper::jsonResponse(true, 'Data Anggota Keluarga Berhasil Diupdate', new FamilyMemberResource($familyMember), 200);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
     }
 
     /**
