351a684133ba2cc951a45c7eb0187a1ed60b98c9
hafifradhitya <bangclasher81@gmail.com>
2025-08-06 19:08:54 +0700

    Membuat private function dan construct di SocialAssistanceController

--- desa-digital-api/app/Http/Controllers/SocialAssistanceController.php
+++ desa-digital-api/app/Http/Controllers/SocialAssistanceController.php
@@ -2,16 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\SocialAssistanceRepositoryInterface;
 use Illuminate\Http\Request;
 
 class SocialAssistanceController extends Controller
 {
+
+    private SocialAssistanceRepositoryInterface $socialAssistanceRepository;
+
+    public function __construct(SocialAssistanceRepositoryInterface $socialAssistanceRepository) {
+        $this->socialAssistanceRepository = $socialAssistanceRepository;
+    }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        try {
+            $socialAssistances = $this->socialAssistanceRepository->getAll(
+                $request->search,
+                $request->limit,
+                true
+            );
+        } catch (\Exception $e) {
+            //throw $th;
+        }
     }
 
     /**
