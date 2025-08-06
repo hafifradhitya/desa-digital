e5ce88fcd7ddcbb6a38b62bd66986b786c5d07a8
hafifradhitya <bangclasher81@gmail.com>
2025-08-06 19:30:43 +0700

    Menambahkan Route social-assistance di web api

--- desa-digital-api/routes/api.php
+++ desa-digital-api/routes/api.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\FamilyMemberController;
 use App\Http\Controllers\HeadOfFamilyController;
+use App\Http\Controllers\SocialAssistanceController;
 use App\Http\Controllers\UserController;
-use App\Models\FamilyMember;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +15,6 @@ Route::get('head-of-family/all/paginated', [HeadOfFamilyController::class, 'getA
 
 Route::apiResource('family-member', FamilyMemberController::class);
 Route::get('family-member/all/paginated', [FamilyMemberController::class, 'getAllPaginated']);
+
+Route::apiResource('social-assistance', SocialAssistanceController::class);
+Route::get('social-assistance/all/paginated', [SocialAssistanceController::class, 'getAllPaginated']);
