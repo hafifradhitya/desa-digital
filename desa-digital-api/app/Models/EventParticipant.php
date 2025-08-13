d896942762ec99f92d9311b39c4e3e5b1a6cd95e
hafifradhitya <bangclasher81@gmail.com>
2025-08-13 09:49:51 +0700

    Buat Data Dummy Event Participant

--- desa-digital-api/app/Models/EventParticipant.php
+++ desa-digital-api/app/Models/EventParticipant.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EventParticipant extends Model
 {
     //
-    use SoftDeletes, UUID;
+    use HasFactory, SoftDeletes, UUID;
 
     protected $fillable = [
         'event_id',
--- desa-digital-api/database/seeders/DatabaseSeeder.php
+++ desa-digital-api/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\EventParticipant;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -21,6 +22,7 @@ class DatabaseSeeder extends Seeder
             SocialAssistanceSeeder::class,
             SocialAssistanceRecipientSeeder::class,
             EventSeeder::class,
+            EventParticipantSeeder::class,
         ]);
     }
 }
