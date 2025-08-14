298e974119eeae04dfdbab8bec2af588d142dbda
hafifradhitya <bangclasher81@gmail.com>
2025-08-14 13:19:16 +0700

    tambah casts di model Development

--- desa-digital-api/app/Models/Development.php
+++ desa-digital-api/app/Models/Development.php
@@ -23,6 +23,10 @@ class Development extends Model
         'status'
     ];
 
+    protected $casts = [
+        'amount' => 'decimal:2'
+    ];
+
     public function scopeSearch($query, $search)
     {
         return $query->where('name', 'like', '%'. $search . '%');
