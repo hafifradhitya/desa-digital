dcec604dfa57f01a7261b97cb7bc046c6d41c2c5
hafifradhitya <bangclasher81@gmail.com>
2025-08-16 15:01:11 +0700

    API Profile get dan create Data

--- desa-digital-api/app/Http/Controllers/ProfileController.php
+++ desa-digital-api/app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ResponseHelper;
+use App\Http\Requests\ProfileStoreRequest;
 use App\Http\Resources\ProfileResource;
 use App\Interfaces\ProfileRepositoryInterface;
 use Illuminate\Http\Request;
@@ -25,6 +26,19 @@ class ProfileController extends Controller
             }
 
             return ResponseHelper::jsonResponse(true, 'Profil Berhasil Diambil', new ProfileResource($profile), 200);
+        } catch (\Exception $e) {
+            return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
+        }
+    }
+
+    public function store(ProfileStoreRequest $request)
+    {
+        $request = $request->validated();
+
+        try {
+            $profile = $this->profileRepository->create($request);
+
+            return ResponseHelper::jsonResponse(true, 'Data Profil Berhasil Ditambahkan', new ProfileResource($profile), 201);
         } catch (\Exception $e) {
             return ResponseHelper::jsonResponse(false, $e->getMessage(), null, 500);
         }
