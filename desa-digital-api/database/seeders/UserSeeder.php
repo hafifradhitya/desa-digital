43c673f7e82404a297c2a308ac9dd5daf9137ccc
hafifradhitya <bangclasher81@gmail.com>
2025-08-16 19:14:24 +0700

    Install dan Konfigurasi Spatie

--- desa-digital-api/database/seeders/UserSeeder.php
+++ desa-digital-api/database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,7 +14,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@app.com',
+            'password' => bcrypt('password')
+        ])->assignRole('admin');
+
         UserFactory::new()->count(15)->create();
     }
 }
